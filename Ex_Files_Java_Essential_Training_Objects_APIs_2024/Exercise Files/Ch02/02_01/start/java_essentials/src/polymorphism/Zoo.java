e8c5e621819ae16738d730b5e6cb3c25f0298633
ajay9177 <ajay098251@gmail.com>
2025-02-26 18:19:55 +0530

    Polymorphism with objects

--- Ex_Files_Java_Essential_Training_Objects_APIs_2024/Exercise Files/Ch02/02_01/start/java_essentials/src/polymorphism/Zoo.java
+++ Ex_Files_Java_Essential_Training_Objects_APIs_2024/Exercise Files/Ch02/02_01/start/java_essentials/src/polymorphism/Zoo.java
@@ -3,6 +3,18 @@ package polymorphism;
 public class Zoo {
 
     public static void main(String[] args) {
+        Dog rocky = new Dog();
+        rocky.fetch();
+        rocky.makeSound();
 
+        Animal sasha = new Dog(); //Sasha is of type Animal but of instance Dog
+        sasha.makeSound();
+
+        sasha = new Cat();
+        sasha.makeSound();
     }
 }
+
+
+//Type vs. Instance - An object can have a superclass type and a sibclass instance
+//Overriden methods = If a method is overriden by the subclass, the polymorphic object will execute the overridden method at runtime
